41d38df9aad1289d041846a753eec9430f814be6
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-09 16:49:52 -0300

    ok
    ok

---
+++ curso-hashtag-js-impressionador/modulo-2/exM02.js
@@ -0,0 +1,26 @@
+
+
+
+
+// -Crie uma variável com o valor inicial igual a 2148. Esse valor não deve ser alterado.
+
+const numero2148 = 2148
+console.log('Resultado do exercício 1 ' + numero2148)
+
+// -Crie uma segunda variável, dessa vez sem um valor inicial.
+
+let semValor = undefined
+console.log('Resultado do exercício 2 ' + semValor)
+
+// -Imprima na tela a seguinte mensagem: "O valor da primeira variável é $valor", substituindo o termo iniciado em $ pelo valor abrigado na sua variável.
+console.log(`O valor da primeira variável é ${numero2148}`)
+
+// -Atribua um valor numérico à segunda variável.
+semValor = 3
+
+console.log(`O resultado do exercicio 4 ` + typeof(semValor))
+
+// -Imprima na tela os textos "Minha segunda variável vale $valor" e "o valor da soma das minhas duas variáveis é $valor", substituindo o termo $valor pelos valores correspondentes.
+
+const soma = semValor + numero2148
+console.log(`Minha segunda variável vale ${semValor} o valor da soma das minhas duas variáveis é ${soma}`)
