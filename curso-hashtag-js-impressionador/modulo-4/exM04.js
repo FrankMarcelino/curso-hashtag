a0e737eca97ac247288c963ff6b3d5a248784c16
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-12 16:06:47 -0300

    terminei

--- curso-hashtag-js-impressionador/modulo-4/exM04.js
+++ curso-hashtag-js-impressionador/modulo-4/exM04.js
@@ -36,7 +36,31 @@ function caulculaMedia() {
 inserirNotas(4,5,6)
 
 
+// //3 - Escreva uma função que recebe do usuário um valor de temperatura em graus Celsius e escreve na
+// tela o texto:
+// a temperatura correspondente em Farenheit é de xºF (sendo x o resultado da conversão.)
+// Obs: temperatura em Farenheit = (9/5)*( temperatura em Celsius ) + 32
 
-// 2 - Imagine agora que o aluno do item anterior não atingiu a média mínima pra passar, e por isso está
-// de prova final. Faça uma nova função que calcula a média final desse aluno a partir de 4 notas, as das 3
-// primeiras provas + a nota da prova final. O cálculo deve ser o seguinte:
+
+function converteParaFerenheit(t) {
+    const fereheit = (9/5)*t+32
+    console.log(fereheit)
+}
+
+converteParaFerenheit(34)
+
+// 4 - Quando estava passeando em Nova York, Bruno se surpreendeu com uma realidade diferente da
+// que conhecia. Ao tentar pagar um produto, ele percebeu que o valor cobrado era sempre um pouco
+// maior do que o que estava presente na etiqueta da prateleira onde o achou. Isso acontece porque a
+// etiqueta não contém o valor do imposto pago sobre o produto, que é calculado no caixa. Esse imposto
+// é de 8,875% em cima do valor do produto. Faça uma função que recebe o valor do produto presente na
+// etiqueta e registre na tela a frase:
+// O valor a ser pago é x (sendo x o resultado do acréscimo dos impostos sobre o valor inicial).
+
+function caulculaImposto(p) {
+    const valorParaSerPago = p + (8.875/100)
+    console.log(`O valor a ser pago é ${valorParaSerPago}`)
+}
+
+caulculaImposto(10)
+
