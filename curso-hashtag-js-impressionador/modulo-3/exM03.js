b3c287023d55f810ccb07f813cdd7181b7557a86
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-12 10:26:23 -0300

    primeiro comentario do dia

---
+++ curso-hashtag-js-impressionador/modulo-3/exM03.js
@@ -0,0 +1,4 @@
+console.log('=================== Exercicio 2 ===============')
+
+// ### 1 - Escreva um programa executando os seguintes passos:
+// -Crie uma variável com o valor inicial igual a uma lista de número de 1 a 5. Esse valor não deve ser alterado.
