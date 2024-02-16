b77173d16e3fc990b6a752bc65209f86a1a05181
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-16 19:52:53 -0300

    feito o 03

--- curso-hashtag-js-impressionador/modulo-5/exM05.js
+++ curso-hashtag-js-impressionador/modulo-5/exM05.js
@@ -5,7 +5,7 @@
 // compra for maior ou igual a R$ 100, o cliente recebe um desconto de 10%. Caso contrário, nenhum desconto
 // é aplicado. O programa deve imprimir mensagens que retornem o se o cliente possui ou não o direito de
 // retorno e o valor da compra no console.
-
+console.log("=============== resolução ex01===========================")
 
 function verificaDesconto(valorCompra) {
     if (valorCompra >= 100) {
@@ -19,10 +19,13 @@ function verificaDesconto(valorCompra) {
 
 verificaDesconto(250)
 
+
+
 // Exercício 2: Verificação de Produto em Estoque:
 // Escreva um programa verificarEstoque que verifica se um produto está em estoque com base na quantidade
 // disponível. Se a quantidade disponível for maior que zero, exiba "Produto disponível", caso contrário, exiba
 // "Produto esgotado" no console
+console.log("=============== resolução ex02===========================")
 
 function verificaEstoque(produto) {
     if(produto > 0) {
@@ -33,3 +36,25 @@ function verificaEstoque(produto) {
 }
 const quantidade = 0
 verificaEstoque(quantidade)
+
+
+// Exercício 3: Determinação de Categoria de Cliente:
+// Escreva um programa categoriaCliente que determine a categoria de um cliente com base em sua
+// pontuação de fidelidade, que será entre 0 e 100. Se a pontuação for maior ou igual a 85, o cliente é
+// categorizado como "Cliente Premium". Se for maior ou igual a 50, é categorizado como "Cliente Regular".
+// Caso contrário, é categorizado como "Cliente Comum".
+console.log("=============== resolução ex03===========================")
+
+function categoriaCliente(pontuacao) {
+    if(pontuacao >= 85){
+        console.log("cliente premium")
+    }else if (pontuacao >= 50){
+        console.log("cliente regular")
+    }else {
+        console.log("cliente comum")
+    }
+}
+
+const pontuacao = 85
+categoriaCliente(pontuacao)
+
