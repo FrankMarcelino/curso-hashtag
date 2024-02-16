8b056855f7beb4baf17878c6d7435821322d25e8
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-16 19:45:49 -0300

    escercio 2 feito

--- curso-hashtag-js-impressionador/modulo-5/exM05.js
+++ curso-hashtag-js-impressionador/modulo-5/exM05.js
@@ -1,14 +1,14 @@
 // JavaScript Impressionador - Módulo 5 - Estruturas Condicionais e de Repetição
 
-// Exercício 1: Verificação de Desconto
+// Exercício 1: Verificação de Desconto:
 // Crie um programa verificaDesconto que verifique se um cliente tem direito a um desconto. Se o valor da
 // compra for maior ou igual a R$ 100, o cliente recebe um desconto de 10%. Caso contrário, nenhum desconto
 // é aplicado. O programa deve imprimir mensagens que retornem o se o cliente possui ou não o direito de
 // retorno e o valor da compra no console.
 
 
-function verificaDesconto(valorCompra){
-    if(valorCompra  >= 100) {
+function verificaDesconto(valorCompra) {
+    if (valorCompra >= 100) {
         console.log(`Parabéns! Você comprou ${valorCompra} reais e recebeu um desconto de 10%`)
         valorDoDesconto = (valorCompra / 100) * 10
         valorFinal = valorCompra - valorDoDesconto
@@ -18,3 +18,18 @@ function verificaDesconto(valorCompra){
 
 
 verificaDesconto(250)
+
+// Exercício 2: Verificação de Produto em Estoque:
+// Escreva um programa verificarEstoque que verifica se um produto está em estoque com base na quantidade
+// disponível. Se a quantidade disponível for maior que zero, exiba "Produto disponível", caso contrário, exiba
+// "Produto esgotado" no console
+
+function verificaEstoque(produto) {
+    if(produto > 0) {
+        console.log(`O produto  está disponivel `)
+    } else {
+        console.log(`O produto  está indisponivel `)
+    }
+}
+const quantidade = 0
+verificaEstoque(quantidade)
