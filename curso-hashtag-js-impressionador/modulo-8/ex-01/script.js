b937b2c393a698ccd704bf37e7e4c0736653d4d9
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-20 15:58:50 -0300

    terminei tudo

---
+++ curso-hashtag-js-impressionador/modulo-8/ex-01/script.js
@@ -0,0 +1,30 @@
+
+const botaoCliqueAqui  = document.querySelector('#button')
+
+function alertaDoBotao() {
+    alert('Botão Clicado!')
+}
+
+botaoCliqueAqui.addEventListener('click',alertaDoBotao)
+
+const inputFocus = document.querySelector('#input')
+
+inputFocus.addEventListener("focus", function() {
+    inputFocus.value =  "Campo em foco"
+});
+
+// Evento de desfoco
+inputFocus.addEventListener("blur", function() {
+    inputFocus.value = ""
+});
+
+const imgMisteriossa = document.querySelector("#img-misteriosa")
+
+function revelaImagem() {
+    imgMisteriossa.setAttribute('src','img2.jpg')
+  }
+
+  // Função que muda a cor para vermelho
+  function ocutaImagemMisteriosa() {
+    imgMisteriossa.setAttribute('src','imagem1.png')
+  }
